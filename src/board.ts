60c078dcec068fa7c7e4f6c4f5401907fa7746bd
OptiGE <optige@gmail.com>
2021-05-07 15:37:28 +0200

    Pieces now have individual shapes

--- src/board.ts
+++ src/board.ts
@@ -22,26 +22,26 @@ export default class Board {
             nonpawns: {}
         }
 
-        this.setUpTeam(this.p1_pieces, 0, 1);
-        this.setUpTeam(this.p2_pieces, 7, -1);
+        this.setUpTeam(this.p1_pieces, [0.02, 0.02, 0.02], 0, 1);
+        this.setUpTeam(this.p2_pieces, [0.5, 0.5, 0.5], 7, -1);
     }
 
-    setUpTeam(team: any, side : TileNumber, offset: number) : void {
+    setUpTeam(team: any, color : [number, number, number], side : TileNumber, offset: number) : void {
         for (let i = 0 as TileNumber; i < 8; i++) {
-            team.pawns[i] = new Piece({a: i, b: (side + offset) as TileNumber}, [0, 0, 1], this.scene)
+            team.pawns[i] = new Piece("pawn", {a: i, b: (side + offset) as TileNumber}, color, this.scene)
         }
 
-        team.nonpawns[0] = new Piece({a: 0, b: side}, [0, 1, 1], this.scene)
-        team.nonpawns[1] = new Piece({a: 7, b: side}, [0, 1, 1], this.scene)
+        team.nonpawns[0] = new Piece("rook", {a: 0, b: side}, color, this.scene)
+        team.nonpawns[1] = new Piece("rook", {a: 7, b: side}, color, this.scene)
 
-        team.nonpawns[2] = new Piece({a: 1, b: side}, [1, 0, 1], this.scene)
-        team.nonpawns[3] = new Piece({a: 6, b: side}, [1, 0, 1], this.scene)
+        team.nonpawns[2] = new Piece("horse", {a: 1, b: side}, color, this.scene)
+        team.nonpawns[3] = new Piece("horse", {a: 6, b: side}, color, this.scene)
 
-        team.nonpawns[4] = new Piece({a: 2, b: side}, [0, 1, 0], this.scene)
-        team.nonpawns[5] = new Piece({a: 5, b: side}, [0, 1, 0], this.scene)
+        team.nonpawns[4] = new Piece("bishop", {a: 2, b: side}, color, this.scene)
+        team.nonpawns[5] = new Piece("bishop", {a: 5, b: side}, color, this.scene)
 
-        team.nonpawns[6] = new Piece({a: 3, b: side}, [0, 0, 0], this.scene)
-        team.nonpawns[7] = new Piece({a: 4, b: side}, [1, 1, 1], this.scene)
+        team.nonpawns[6] = new Piece("king", {a: 3, b: side}, color, this.scene)
+        team.nonpawns[7] = new Piece("queen", {a: 4, b: side}, color, this.scene)
     }
 
 }
--- src/piece.ts
+++ src/piece.ts
@@ -9,11 +9,11 @@ export default class Piece {
     private mesh: BABYLON.Mesh;
 
 
-    constructor(position : TilePosition, color : [number, number, number], scene : BABYLON.Scene) {
+    constructor(pieceType: string, position : TilePosition, color : [number, number, number], scene : BABYLON.Scene) {
         this.scene = scene;
         this.position = position;
 
-        this.createPiece(color);
+        this.createPiece(pieceType, color);
         this.movePiece(position);
     }
 
@@ -24,14 +24,46 @@ export default class Piece {
         this.mesh.position.z = absPos.z;
     }
 
-    createPiece([r, g, b]: [number, number, number]) : void{
-            this.mesh = BABYLON.Mesh.CreateBox("white", boardSize/12, this.scene);
+    createPiece(pieceType : string, [r, g, b]: [number, number, number]) : void{
+        let height = boardSize/20;
+
+        switch(pieceType) {
+            case "pawn":
+                height = boardSize/20;
+                this.mesh = BABYLON.MeshBuilder.CreateBox("white", {"height": height, "width": boardSize/20, "depth": boardSize/20}, this.scene);
+            break;
+            case "rook":
+                height = boardSize/10;
+                this.mesh = BABYLON.MeshBuilder.CreateBox("white", {"height": height, "width": boardSize/14, "depth": boardSize/14}, this.scene);
+            break;
+            case "bishop":
+                height = 45;
+                this.mesh = BABYLON.MeshBuilder.CreatePolyhedron("white", {"type": 1, "size": boardSize/25}, this.scene);
+            break;
+            case "horse":
+                height = 10;
+                this.mesh = BABYLON.MeshBuilder.CreateTorus("white", {"diameter": 25, "thickness": 10} , this.scene);
+            break;
+            case "king":
+                height = boardSize/6;
+                this.mesh = BABYLON.MeshBuilder.CreateCylinder("white", {"height": height, "diameterBottom": 30, "diameterTop": 30}, this.scene);
+            break;
+            case "queen":
+                height = boardSize/5;
+                this.mesh = BABYLON.MeshBuilder.CreateCylinder("white", {"height": height, "diameterBottom": 30, "diameterTop": 4}, this.scene);
+            break;
+            default:
+                height = boardSize/20;
+                this.mesh = BABYLON.MeshBuilder.CreateBox("white", {"height": height, "width": boardSize/20, "depth": boardSize/20}, this.scene);
+            break;
+        }
+
             var blueMat = new BABYLON.StandardMaterial("ground", this.scene);
             blueMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
             blueMat.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4);
             blueMat.emissiveColor = new BABYLON.Color3(r, g, b);
             this.mesh.material = blueMat;
-            this.mesh.position.y = boardSize/12/2;
+            this.mesh.position.y = height/2;
     }
 
 }
