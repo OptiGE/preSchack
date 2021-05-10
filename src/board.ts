16a383ef9fe0cdf2b2584ba494a5a4d47534c047
OptiGE <34107649+OptiGE@users.noreply.github.com>
2021-05-10 20:21:08 +0200

    Piece mesh cleanup (#2)
    * Technically working, although ugly solution. Will need to be rebuilt for animation

--- src/board.ts
+++ src/board.ts
@@ -6,8 +6,8 @@ import BoardStateHandler from './utilities/boardStateHandler';
 
 export default class Board {
     private scene: BABYLON.Scene;
-    private p1_pieces: any;
-    private p2_pieces: any;
+    private p1_pieces: Piece[];
+    private p2_pieces: Piece[];
     private boardStateHandler: BoardStateHandler;
 
     constructor(scene : BABYLON.Scene) {
@@ -19,18 +19,28 @@ export default class Board {
     }
 
     placePieces(){
-        //MAN MÅSTE JU LOOPA ÖVER VALUES SPECIFIKT
-        //ELLER KANSKE TOM KEYS OCH VALUES FÖR ATT HÅLLA KOLL PÅ VART
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+        this.clearPieces();
         let board = this.boardStateHandler.getBoard()
 
         for (const [number, row] of Object.entries(board)) {
             for (const [letter, piece] of Object.entries(row)) {
                 if (piece == "") continue;
-                console.log(piece)
                 this.p1_pieces.push(new Piece(piece.charAt(1), {l: letter as TileLetter, n: parseInt(number) as TileNumber}, piece.charAt(0), this.scene))
             }
         }
+    }
+
+    clearPieces(){
+        this.p1_pieces.forEach(piece => {
+            piece.deleteMesh();
+        });
+
+        this.p2_pieces.forEach(piece => {
+            piece.deleteMesh();
+        });
+
+        this.p1_pieces = [];
+        this.p2_pieces = [];
     }
 
     updateBoard(update){
