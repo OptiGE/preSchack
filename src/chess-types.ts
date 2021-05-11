89a9eb65c4a97cfb8ad5a20c20d71e3695ca33bf
OptiGE <34107649+OptiGE@users.noreply.github.com>
2021-05-11 18:50:31 +0200

    Add vr movement (#3)
    * Technically functional

--- src/chess-types.ts
+++ src/chess-types.ts
@@ -1,7 +1,7 @@
 export type TileLetter =  "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
 export type TileNumber =   1  |  2  |  3  |  4  |  5  |  6  |  7  |  8 ;
 
-export const boardSize = 400;
+export const boardSize = 10;
 
 export interface HumanPosition {
     l : TileLetter;
