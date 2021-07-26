91924bc156cb3fa12ae81458b97d74b4ccb0ea5f
LucasBackvall <lucasba@student.chalmers.se>
2021-07-26 21:40:35 +0200

    Refactored gamestate handler (#7)
    - Includes some linter fixes

--- src/chess-types.ts
+++ src/chess-types.ts
@@ -1,39 +1,6 @@
-export type TileLetter =  "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
-export type TileNumber =   1  |  2  |  3  |  4  |  5  |  6  |  7  |  8 ;
-
-export const boardSize = 10;
-
-export interface HumanPosition {
-    l : TileLetter;
-    n : TileNumber;
-}
+export const boardSize = 5
 
 export interface AbsPosition {
-    x : number;
-    z : number;
-}
-
-//Båda nedanstående är type istället för interface pga: https://github.com/microsoft/TypeScript/issues/24220
-export type BoardState = {
-    [rowNumber in TileNumber] : BoardRow
-}
-
-export type BoardRow = {
-    [colLetter in TileLetter] : string
-}
-
-export function getAbsCoord(humanpos : HumanPosition) : AbsPosition {
-    let cornerxz = (boardSize / 2) - (boardSize / 16)
-    let tileSize = boardSize / 8
-    
-    let abspos : AbsPosition = {
-        x: cornerxz - tileSize * (getNumFromLetter(humanpos.l) - 1),
-        z: cornerxz - tileSize * (humanpos.n - 1)
-    }
-
-    return abspos
-}
-
-export function getNumFromLetter(l : TileLetter){
-    return l.toLowerCase().charCodeAt(0) - 97 + 1
+    x: number;
+    z: number;
 }
--- src/piece.ts
+++ src/piece.ts
@@ -1,73 +1,89 @@
-import * as BABYLON from 'babylonjs';
+import * as BABYLON from 'babylonjs'
 
-import { TileLetter, TileNumber, HumanPosition, AbsPosition, boardSize, getAbsCoord } from './chess-types'
+import { Coordinate, PieceValue } from './gamestate/gamestateTypes'
+import { AbsPosition, boardSize } from './chess-types'
 
 export default class Piece {
     private scene: BABYLON.Scene;
-    private position: HumanPosition;
+    private position: Coordinate;
+    private pieceValue: PieceValue;
     private mesh: BABYLON.Mesh;
 
-
-    constructor(pieceType: string, position : HumanPosition, color : string, scene : BABYLON.Scene) {
-        this.scene = scene;
-        this.position = position;
-
-        this.createPiece(pieceType, color);
-        this.movePiece(position);
+    constructor (position: Coordinate, pieceValue: PieceValue, scene: BABYLON.Scene) {
+      this.scene = scene
+      this.position = position
+      this.pieceValue = pieceValue
+      this.createPiece()
     }
 
-    movePiece(newpos : HumanPosition) : void {
-        let absPos = getAbsCoord(newpos)
-        
-        this.mesh.position.x = absPos.x;
-        this.mesh.position.z = absPos.z;
+    public deleteMesh (): void {
+      this.mesh.dispose()
     }
 
-    createPiece(pieceType : string, color : string) : void{
-        let height = boardSize/20;
+    private movePiece (): void {
+      const absPos = this.getAbsCoord(this.position)
 
-        switch(pieceType) {
-            case "p":
-                height = boardSize/20;
-                this.mesh = BABYLON.MeshBuilder.CreateBox("gray", {"height": height, "width": boardSize/20, "depth": boardSize/20}, this.scene);
-            break;
-            case "r":
-                height = boardSize/10;
-                this.mesh = BABYLON.MeshBuilder.CreateBox("gray", {"height": height, "width": boardSize/14, "depth": boardSize/14}, this.scene);
-            break;
-            case "b":
-                height = boardSize/10;
-                this.mesh = BABYLON.MeshBuilder.CreatePolyhedron("gray", {"type": 1, "size": boardSize/25}, this.scene);
-            break;
-            case "k":
-                height = boardSize/30;
-                this.mesh = BABYLON.MeshBuilder.CreateTorus("gray", {"diameter": boardSize/15, "thickness": boardSize/30} , this.scene);
-            break;
-            case "K":
-                height = boardSize/6;
-                this.mesh = BABYLON.MeshBuilder.CreateBox("gray", {"height": height, "width": boardSize/20, "depth": boardSize/20}, this.scene);
-            break;
-            case "q":
-                height = boardSize/5;
-                this.mesh = BABYLON.MeshBuilder.CreateCylinder("gray", {"height": height, "diameterBottom": boardSize/10, "diameterTop": boardSize/35}, this.scene);
-            break;
-            default:
-                height = boardSize/20;
-                this.mesh = BABYLON.MeshBuilder.CreateBox("gray", {"height": height, "width": boardSize/20, "depth": boardSize/20}, this.scene);
-            break;
-        }
-
-            var blueMat = new BABYLON.StandardMaterial("ground", this.scene);
-            blueMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-            blueMat.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-            if(color == "w")       blueMat.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-            else if (color == "b") blueMat.emissiveColor = new BABYLON.Color3(0.02, 0.02, 0.02);
-            this.mesh.material = blueMat;
-            this.mesh.position.y = height/2;
+      this.mesh.position.x = absPos.x
+      this.mesh.position.z = absPos.z
     }
 
-    deleteMesh() : void{
-        this.mesh.dispose();
+    private createPiece (): void {
+      let height = boardSize / 20
+
+      switch (this.pieceValue.charAt(1)) {
+        case 'p':
+          height = boardSize / 20
+          this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 20, depth: boardSize / 20 }, this.scene)
+          break
+        case 'r':
+          height = boardSize / 10
+          this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 14, depth: boardSize / 14 }, this.scene)
+          break
+        case 'b':
+          height = boardSize / 10
+          this.mesh = BABYLON.MeshBuilder.CreatePolyhedron('gray', { type: 1, size: boardSize / 25 }, this.scene)
+          break
+        case 'k':
+          height = boardSize / 30
+          this.mesh = BABYLON.MeshBuilder.CreateTorus('gray', { diameter: boardSize / 15, thickness: boardSize / 30 }, this.scene)
+          break
+        case 'K':
+          height = boardSize / 6
+          this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 20, depth: boardSize / 20 }, this.scene)
+          break
+        case 'q':
+          height = boardSize / 5
+          this.mesh = BABYLON.MeshBuilder.CreateCylinder('gray', { height: height, diameterBottom: boardSize / 10, diameterTop: boardSize / 35 }, this.scene)
+          break
+        default:
+          console.error('Undefined piece type')
+          break
+      }
+
+      const blueMat = new BABYLON.StandardMaterial('ground', this.scene)
+      blueMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4)
+      blueMat.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4)
+      if (this.pieceValue.charAt(0) === 'w') blueMat.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5)
+      else if (this.pieceValue.charAt(0) === 'b') blueMat.emissiveColor = new BABYLON.Color3(0.02, 0.02, 0.02)
+      this.mesh.material = blueMat
+      this.mesh.position.y = height / 2
+
+      this.movePiece()
     }
 
+    private getAbsCoord (coordinate: Coordinate): AbsPosition {
+      const cornerxz = (boardSize / 2) - (boardSize / 16)
+      const tileSize = boardSize / 8
+
+      const abspos: AbsPosition = {
+        x: cornerxz - tileSize * (this.getNumFromLetter(coordinate.charAt(0)) - 1),
+        z: cornerxz - tileSize * (parseInt(coordinate.charAt(1)) - 1)
+      }
+
+      return abspos
+    }
+
+    private getNumFromLetter (l: string) : number {
+      return l.toLowerCase().charCodeAt(0) - 97 + 1
+    }
 }
