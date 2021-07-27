3d2377c4c65191f0dc81f4dad6b0cae1eed7ad95
LucasBackvall <lucasba@student.chalmers.se>
2021-07-27 22:11:25 +0200

    Changed linter options to more reasonable values than default. (#9)

--- src/absPosition.ts
+++ src/absPosition.ts
@@ -1,4 +1,6 @@
-export default interface AbsPosition {
+interface AbsPosition {
     x: number;
     z: number;
 }
+
+export default AbsPosition;
--- src/coordinateConverter.ts
+++ src/coordinateConverter.ts
@@ -1,34 +1,34 @@
-import { Coordinate } from './gamestate/gamestateTypes'
-import { boardSize } from './const'
-import AbsPosition from './absPosition'
+import { Coordinate } from './gamestate/gamestateTypes';
+import { boardSize } from './const';
+import AbsPosition from './absPosition';
 
 export function getAbsCoord (coordinate: Coordinate): AbsPosition {
-  const cornerxz = (boardSize / 2) - (boardSize / 16)
-  const tileSize = boardSize / 8
+    const cornerxz = (boardSize / 2) - (boardSize / 16);
+    const tileSize = boardSize / 8;
 
-  const abspos: AbsPosition = {
-    x: cornerxz - tileSize * (getNumFromLetter(coordinate.charAt(0)) - 1),
-    z: cornerxz - tileSize * (parseInt(coordinate.charAt(1)) - 1)
-  }
+    const abspos: AbsPosition = {
+        x: cornerxz - tileSize * (getNumFromLetter(coordinate.charAt(0)) - 1),
+        z: cornerxz - tileSize * (parseInt(coordinate.charAt(1)) - 1)
+    };
 
-  return abspos
+    return abspos;
 }
 
 export function getHumanCoord (abspos : AbsPosition) : Coordinate {
-  const cornerxz = (boardSize / 2) - (boardSize / 16)
-  const tileSize = boardSize / 8
+    const cornerxz = (boardSize / 2) - (boardSize / 16);
+    const tileSize = boardSize / 8;
 
-  const numAboutToBeLetterletter = Math.round((cornerxz - abspos.x) / tileSize) // Avstånd från hörnet till faktisk position delat på rutbredd = antal rutor
-  const letter = getLetterFromNum(numAboutToBeLetterletter)
-  const number = ((cornerxz - abspos.z) / tileSize) + 1 // +1 för att det är nollindexerat tror
+    const numAboutToBeLetterletter = Math.round((cornerxz - abspos.x) / tileSize); // Avstånd från hörnet till faktisk position delat på rutbredd = antal rutor
+    const letter = getLetterFromNum(numAboutToBeLetterletter);
+    const number = ((cornerxz - abspos.z) / tileSize) + 1; // +1 för att det är nollindexerat tror
 
-  return `${letter}${number}` as Coordinate
+    return `${letter}${number}` as Coordinate;
 }
 
 export function getNumFromLetter (l: string) : number {
-  return l.toLowerCase().charCodeAt(0) - 97 + 1
+    return l.toLowerCase().charCodeAt(0) - 97 + 1;
 }
 
 export function getLetterFromNum (n : number): string {
-  return String.fromCharCode(n + 97).toLowerCase()
+    return String.fromCharCode(n + 97).toLowerCase();
 }
