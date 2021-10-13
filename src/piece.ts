fb86a4b598048ef9229ea282125391d4ac8aea50
OptiGE <optige@gmail.com>
2021-10-13 22:12:48 +0200

    Finally have control of the meshes!

--- src/piece.ts
+++ src/piece.ts
@@ -4,6 +4,7 @@ import 'babylonjs-loaders';
 import { Coordinate, PieceValue } from './gamestate/gamestateTypes';
 import { getAbsCoord } from './coordinateConverter';
 import { boardSize } from './const';
+import { Mesh } from 'babylonjs/Meshes/mesh';
 
 export default class Piece {
     private scene: BABYLON.Scene;
@@ -36,50 +37,67 @@ export default class Piece {
     }
 
     private createPiece(): void {
+        const self = this;
         let height = boardSize / 20;
+
+        const assetsManager = new BABYLON.AssetsManager(this.scene);
+        assetsManager.load();
 
         switch (this.pieceValue.charAt(1)) {
             case 'p':
                 //this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 20, depth: boardSize / 20 }, this.scene);
-
                 // The first parameter can be used to specify which mesh to import. Here we import all meshes
-                BABYLON.SceneLoader.ImportMesh('', './src/models/low_poly_pawn/', 'scene.glb', this.scene, function (container) {
-                    console.log("Container");
-                    console.log(container);
+                BABYLON.SceneLoader.ImportMesh('', './src/models/low_poly_pawn/', 'scene.glb', this.scene, function (meshes) {
+                    console.log(meshes);
+                    self.mesh = meshes[0];
+                    self.mesh.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+                    self.addMaterial(height);
+                    self.movePiece();
                 });
 
-                //Try to use babylonjs.loaders.js instead of babylon.glTF1FileLoader.js as your file is probably not a gltf1 file.
-                // https://forum.babylonjs.com/t/unable-to-import-mesh/20298/12
-
-                // import 'babylonjs-loaders';
-
-
-                break;
-            case 'r':
-                height = boardSize / 10;
-                this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 14, depth: boardSize / 14 }, this.scene);
-                break;
-            case 'b':
-                height = boardSize / 10;
-                this.mesh = BABYLON.MeshBuilder.CreatePolyhedron('gray', { type: 1, size: boardSize / 25 }, this.scene);
-                break;
-            case 'k':
-                height = boardSize / 30;
-                this.mesh = BABYLON.MeshBuilder.CreateTorus('gray', { diameter: boardSize / 15, thickness: boardSize / 30 }, this.scene);
-                break;
-            case 'K':
-                height = boardSize / 6;
-                this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 20, depth: boardSize / 20 }, this.scene);
-                break;
-            case 'q':
-                height = boardSize / 5;
-                this.mesh = BABYLON.MeshBuilder.CreateCylinder('gray', { height: height, diameterBottom: boardSize / 10, diameterTop: boardSize / 35 }, this.scene);
-                break;
+            /*
+        let meshTask = assetsManager.addMeshTask("pawn", "", "./src/models/low_poly_pawn/", "scene.glb");
+        meshTask.onSuccess = function (task) {
+            task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
+            task.loadedMeshes[0].scaling.copyFromFloats(0.03, 0.03, 0.03);
+            let fishMesh = task.loadedMeshes[0];
+            fishMesh.rotationQuaternion = new BABYLON.Quaternion();
+            fishMesh.position.y -= 1.2;
+        }
+        messTask.onError = (task) => { console.log("HLEP"); }
+        break;
+        */
+            /*
+                case 'r':
+                    height = boardSize / 10;
+                    this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 14, depth: boardSize / 14 }, this.scene);
+                    break;
+                case 'b':
+                    height = boardSize / 10;
+                    this.mesh = BABYLON.MeshBuilder.CreatePolyhedron('gray', { type: 1, size: boardSize / 25 }, this.scene);
+                    break;
+                case 'k':
+                    height = boardSize / 30;
+                    this.mesh = BABYLON.MeshBuilder.CreateTorus('gray', { diameter: boardSize / 15, thickness: boardSize / 30 }, this.scene);
+                    break;
+                case 'K':
+                    height = boardSize / 6;
+                    this.mesh = BABYLON.MeshBuilder.CreateBox('gray', { height: height, width: boardSize / 20, depth: boardSize / 20 }, this.scene);
+                    break;
+                case 'q':
+                    height = boardSize / 5;
+                    this.mesh = BABYLON.MeshBuilder.CreateCylinder('gray', { height: height, diameterBottom: boardSize / 10, diameterTop: boardSize / 35 }, this.scene);
+                    break;
+            */
             default:
                 console.error('Undefined piece type');
                 break;
         }
+    }
 
+    private addMaterial(height: number): void {
+        console.log("this");
+        console.log(this);
         this.material = new BABYLON.StandardMaterial('ground', this.scene);
         this.material.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
         this.material.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4);
@@ -87,8 +105,6 @@ export default class Piece {
         else if (this.pieceValue.charAt(0) === 'b') this.material.emissiveColor = new BABYLON.Color3(0.02, 0.02, 0.02);
         this.mesh.material = this.material;
         this.mesh.position.y = height / 2;
-
-        this.movePiece();
     }
 
     public toggleSelect(): void {
